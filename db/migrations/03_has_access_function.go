dfdfec4ba6c4c035ea6b3b2f9bbe7f331c7d35b1
strobus <22136798+strobus@users.noreply.github.com>
2022-08-03 01:47:28 -0400

    add superadmin case

--- db/migrations/03_has_access_function.go
+++ db/migrations/03_has_access_function.go
@@ -24,11 +24,12 @@ CREATE OR REPLACE FUNCTION %[1]s.has_access (
             SELECT *
             FROM %[1]s.casbin_rules r
             LEFT JOIN roles ON r.v0 = roles.role
-            WHERE r.p_type = 'p'
+            WHERE (r.p_type = 'p'
                 AND (r.v0 = user_id OR r.v0 = roles.role OR (r.v0 = 'USER' AND roles.role = 'ADMIN'))
                 AND (r.v1 = domain OR (position('*' in r.v1) > 0 AND starts_with(domain, rtrim(r.v1, '*'))))
-                AND r.v2 LIKE ANY(resources)
-                AND (r.v3 = '*' OR r.v3 = action OR (r.v3 = 'update' AND action = 'read'))
+                AND r.v2 = ANY(resources)
+                AND (r.v3 = '*' OR r.v3 = action OR (r.v3 = 'update' AND action = 'read')))
+                 OR roles.role = 'SUPERADMIN'
         );
     END;
 $$ LANGUAGE plpgsql
